c6b866630b2d06bfa25b5898ba3eefcd334edec3
zakirovtech <zakirovdiy@gmail.com>
2025-03-22 10:50:12 +0300

    Separate certbot

--- src/certbot/init.sh
+++ src/certbot/init.sh
@@ -1,4 +1,11 @@
-certbot certonly --webroot --webroot-path /var/www/certbot \
-    -d $SITE_DOMAIN -d www.$SITE_DOMAIN \
-    --email $ADMIN_EMAIL \
-    --agree-tos --non-interactive
+echo "Update https certs..."
+
+docker exec --env-file ../env/.env certbot renew --webroot \
+    --webroot-path /var/www/certbot --non-interactive --quiet
+
+if [ $? -ne 0 ]; then
+    echo "Certbot  can not find certs, so try to create new..."
+    docker exec --env-file ../env/.env certbot certonly --webroot \
+        --webroot-path /var/www/certbot -d $SITE_DOMAIN -d www.$SITE_DOMAIN \
+        --email $ADMIN_EMAIL --agree-tos --non-interactive
+fi
