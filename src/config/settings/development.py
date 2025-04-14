35c57f96bacbe712cdabdfd0b390931fbb8d764c
zakirovtech <zakirovdiy@gmail.com>
2025-04-14 15:48:29 +0300

    Init postgres replication logic in dev environment

--- src/config/settings/development.py
+++ src/config/settings/development.py
@@ -1,6 +1,7 @@
 import sentry_sdk
 
 from config.settings.base import *
+from config.utils import is_postgres_available
 
 DEBUG = True
 
@@ -15,14 +16,59 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8080",
 ]
 
+# You can use the code bellow in production settings, when you need to scale 
+# db servers with master and replicas. 
+# |
+# |
+# V
 
-# DATABASES
-DATABASES = {
+# DATABASES AND REPLICATION
+db_creds = {
+    "ENGINE": "django.db.backends.postgresql",
+    "NAME": config("POSTGRES_DB"),
+    "USER": config("POSTGRES_USER"),
+    "PASSWORD": config("POSTGRES_PASSWORD"),
+    "HOST": "192.168.0.100", # localhost MasterDB
+    "PORT": config("DB_PORT"),
+}
+
+SQLITE3 = None
+
+if not is_postgres_available(credentials=db_creds):
+    print("[DEBUG] SQLITE3 IS IN USING")
+    
+    SQLITE3 = True
+
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+else:
+    DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": "192.168.0.100", # localhost MasterDB
+        "PORT": config("DB_PORT"),
+
+    },
+    "replica": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": "192.168.0.101", # localhost ReplicaDB1
+        "PORT": config("DB_PORT"),
+        
     }
 }
+
+if not SQLITE3:    
+    DATABASE_ROUTERS = ["config.dbrouting.ReadWriteRouter"]
 
 # CACHE
 CACHES = {
